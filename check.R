546ed2ac90b619934d1d3b4a835a17b09a52b6f0
Rob J Hyndman <robjhyndman@gmail.com>
2022-01-10 15:01:08 +1100

    Added check script

---
+++ check.R
@@ -0,0 +1,23 @@
+## Check ctv is correct
+
+library(ctv)
+
+ctv <- "TimeSeries"
+ctvfile <- here::here(paste0(ctv, ".md"))
+htmlfile <- here::here(paste0(ctv, ".html"))
+
+# Set CRAN mirror
+r <- getOption("repos") 
+r["CRAN"] <- "https://cloud.r-project.org"
+options(repos = r)
+
+# Run the check
+check_ctv_packages(ctvfile) 
+
+# Create html file from ctv file
+ctv2html(read.ctv(ctvfile), htmlfile)
+
+# View html file
+browseURL(htmlfile)
+
+cat("Done.\n")
